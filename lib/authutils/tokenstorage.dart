32eaf4d48516a552ca12a95edef5e1e48e09001f
Edoardo Remondini <edoardoremondini@studenti.itsincom.it>
2021-12-21 18:52:01 +0100

    aggiunta classe per gestione token in memoria

---
+++ lib/authutils/tokenstorage.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class Tokenstorage {
+  static final FlutterSecureStorage _storage = FlutterSecureStorage(
+    aOptions: AndroidOptions(
+        encryptedSharedPreferences: true), //così dati salvati sono criptati
+  );
+
+  static Future<void> savetoken(String key, String value) async =>
+      await _storage.write(key: key, value: value);
+
+  static Future<String?> retrievetokenvalue(String key) async =>
+      await _storage.read(key: key);
+
+  static Future<void> removetokenfromstorage(String key) async {
+    //logout
+    await _storage.delete(key: key);
+  }
+}
