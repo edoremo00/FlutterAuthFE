b63fc990c30317e4e0b481639acec4f9fb9a03ed
Edoardo Remondini <edoardoremondini@studenti.itsincom.it>
2021-12-27 20:37:38 +0100

    aggiunta pagina settings per gestione profilo utente loggato

---
+++ lib/screens/settingspage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Settingspage extends StatelessWidget {
+  const Settingspage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
