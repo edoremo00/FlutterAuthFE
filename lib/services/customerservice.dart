a07dab2b902e9776bea03c12d5ad28a2b7053176
Edoardo Remondini <edoardoremondini@studenti.itsincom.it>
2022-01-02 01:07:03 +0100

    aggiunta chiamata API per modifica utente e caricamento foto profilo

--- lib/services/customerservice.dart
+++ lib/services/customerservice.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 import 'package:authapi/models/customermodel.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:image_picker/image_picker.dart';
 
 const String customerbaseurl = "http://10.0.2.2:5000/api/Customer";
+const String uploadimagebaseurl =
+    "http://10.0.2.2:5000/api/UploadimagefileFlutter";
 
 Future<List<Customermodel>> Getall() async {
   Response r = await http.get(Uri.parse('$customerbaseurl/Getallcustomers'));
@@ -32,5 +35,35 @@ Future<bool> Deletecustomer({required String idcustomer}) async {
     return response;
   } else {
     return false;
+  }
+}
+
+Future<bool> UpdateCustomer({required Customermodel model}) async {
+  Response r = await http.put(Uri.parse('$customerbaseurl/Updatecustomer'),
+      body: model.toJson(),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json'
+      });
+  if (r.statusCode == 200) {
+    bool response = r.body == 'true';
+    return response;
+  } else {
+    return false;
+  }
+}
+
+Future<StreamedResponse> Uploadprofilepicture(
+    {required XFile file, required String username}) async {
+  MultipartRequest request = http.MultipartRequest(
+    'POST',
+    Uri.parse(
+        '$uploadimagebaseurl/Uploadprofilepicturetoblobstorage/$username'),
+  )..files.add(await MultipartFile.fromPath('profilepicture', file.path));
+  StreamedResponse response = await request.send();
+  if (response.statusCode == 200) {
+    return response;
+  } else {
+    return response;
   }
 }
