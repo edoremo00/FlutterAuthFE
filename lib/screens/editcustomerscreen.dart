01b4c41a531a0a2a63812b2b722ef6ca380b069b
Edoardo Remondini <edoardoremondini@studenti.itsincom.it>
2022-01-02 01:06:26 +0100

    update parte solo se form è stato cambiato. per questo motivo è stato aggiunto un nuovo oggetto che viene passato come body alle chiamate HTTP.+ aggiunto metodo validazione estensione file + metodo caricamento file e passaggio dati nuovi tramite navigator per poi aggiornare la UI

--- lib/screens/editcustomerscreen.dart
+++ lib/screens/editcustomerscreen.dart
@@ -1,6 +1,11 @@
 import 'package:authapi/models/customermodel.dart';
+import 'package:authapi/services/customerservice.dart';
+import 'package:authapi/utils/snackbar.dart';
 import 'package:authapi/widgets/loginform.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 class editcustomerscreen extends StatefulWidget {
   final Customermodel toupdate;
@@ -12,13 +17,19 @@ class editcustomerscreen extends StatefulWidget {
 }
 
 class _editcustomerscreenState extends State<editcustomerscreen> {
+  Customermodel newone = Customermodel();
+  bool updatinguserinfo = false;
+  bool cameraselected = false;
+  final ImagePicker _picker = ImagePicker();
   final _formkey = GlobalKey<FormState>();
+  late TextEditingController namecontroller;
   late TextEditingController usernamecontroller;
   late TextEditingController surnamecontroller;
   late TextEditingController emailcontroller;
   @override
   void initState() {
     super.initState();
+    namecontroller = TextEditingController(text: widget.toupdate.name);
     usernamecontroller = TextEditingController(text: widget.toupdate.username);
     surnamecontroller = TextEditingController(text: widget.toupdate.surname);
     emailcontroller = TextEditingController(text: widget.toupdate.email);
@@ -26,6 +37,7 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
 
   @override
   void dispose() {
+    namecontroller.dispose();
     usernamecontroller.dispose();
     surnamecontroller.dispose();
     emailcontroller.dispose();
@@ -38,11 +50,50 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
       appBar: AppBar(
         actions: [
           TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Salva',
-              style: TextStyle(color: Colors.white),
-            ),
+            onPressed: () async {
+              int valueschanged = userchangedvalues();
+              if (valueschanged == 0) {
+                Utils.showsnackbar(context, 'please modify at least one field');
+              }
+              copyoldifnull(newone);
+              if (_formkey.currentState?.validate() ?? false) {
+                setState(() {
+                  updatinguserinfo = true;
+                });
+                bool updateresponse =
+                    await UpdateCustomer(model: newone).whenComplete(
+                  () => setState(() {
+                    updatinguserinfo = false;
+                  }),
+                );
+                if (updateresponse) {
+                  Utils.showsnackbar(
+                      context, '${newone.username} updated successfully');
+                  await Future.delayed(const Duration(seconds: 3)).then(
+                      (value) => ScaffoldMessenger.of(context)
+                          .removeCurrentSnackBar());
+                  Navigator.pop(context, newone);
+                } else {
+                  Utils.showsnackbar(
+                    context,
+                    'an error occured while updating',
+                    backrgoundcolor: Colors.red,
+                    icon: Icons.error,
+                    iconcolor: Colors.black,
+                  );
+                }
+              } else {
+                return;
+              }
+            },
+            child: updatinguserinfo
+                ? const CircularProgressIndicator(
+                    color: Colors.white,
+                  )
+                : const Text(
+                    'Salva',
+                    style: TextStyle(color: Colors.white),
+                  ),
           ),
         ],
         title: const Text(
@@ -67,13 +118,46 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
                         height: 80,
                         child: InkWell(
                           onTap: () async {
-                            await bottomsheet(context);
+                            bool? uploadchoice = await bottomsheet(context);
+                            if (uploadchoice != null) {
+                              XFile? selectedpicture = await _picker.pickImage(
+                                source: uploadchoice
+                                    ? ImageSource.camera
+                                    : ImageSource.gallery,
+                              );
+                              if (selectedpicture != null) {
+                                if (validatefileextension(selectedpicture)) {
+                                  copyoldifnull(newone);
+                                  StreamedResponse respnse =
+                                      await Uploadprofilepicture(
+                                          file: selectedpicture,
+                                          username: newone.username!);
+                                  if (respnse.reasonPhrase == 'OK') {
+                                    String newimageurl =
+                                        await respnse.stream.bytesToString();
+                                    setState(() {
+                                      //aggiorna la UI in pagina corrente
+                                      widget.toupdate.imagelink = newimageurl;
+                                      newone.imagelink = newimageurl;
+                                    });
+                                    Navigator.of(context).pop(newone);
+                                    //passo il nuovo valore alla pagina precedente che aggiornerà anche lei la UI
+                                  }
+                                  //CHIAMATA API
+                                } else {
+                                  return; //file non valido
+                                }
+                              } else {
+                                return;
+                              }
+                            }
                           },
                         ),
                         image: widget.toupdate.imagelink == "" ||
                                 widget.toupdate.imagelink == null
                             ? const NetworkImage(
-                                'https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png')
+                                'https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png',
+                              )
                             : NetworkImage(
                                 widget.toupdate.imagelink!,
                               ),
@@ -90,7 +174,38 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
             Center(
               child: GestureDetector(
                 onTap: () async {
-                  await bottomsheet(context);
+                  bool? uploadphotochoice = await bottomsheet(context);
+                  if (uploadphotochoice != null) {
+                    XFile? chosenpicture = await _picker.pickImage(
+                      source: uploadphotochoice
+                          ? ImageSource.camera
+                          : ImageSource.gallery,
+                    );
+                    if (chosenpicture != null) {
+                      if (validatefileextension(chosenpicture)) {
+                        //chiamata api
+                        copyoldifnull(newone);
+                        StreamedResponse respnse = await Uploadprofilepicture(
+                            file: chosenpicture, username: newone.username!);
+                        if (respnse.reasonPhrase == 'OK') {
+                          String newimageurl =
+                              await respnse.stream.bytesToString();
+                          setState(() {
+                            widget.toupdate.imagelink = newimageurl;
+                            newone.imagelink = newimageurl;
+                          });
+                          Navigator.of(context).pop(newone);
+                          //passo il nuovo valore alla pagina precedente che aggiornerà anche lei la UI
+                        }
+                      } else {
+                        return; //file non valido
+                      }
+                    } else {
+                      return; //file non selezionato
+                    }
+                  } else {
+                    return;
+                  }
                 },
                 child: const Text(
                   'Cambia immagine del profilo',
@@ -102,6 +217,9 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
               ),
             ),
             loginform(
+              onchanged: (username) {
+                newone.username = usernamecontroller.text;
+              },
               validator: (username) {
                 if (username!.isEmpty) {
                   return 'il campo username non può essere vuoto';
@@ -121,6 +239,9 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
               height: 16,
             ),
             loginform(
+              onchanged: (surname) {
+                newone.surname = surnamecontroller.text;
+              },
               autovalidateMode: AutovalidateMode.onUserInteraction,
               validator: (surname) {
                 if (surname!.isEmpty) {
@@ -140,6 +261,28 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
               height: 16,
             ),
             loginform(
+              onchanged: (name) {
+                newone.name = namecontroller.text;
+              },
+              labeltext: 'Name',
+              controller: namecontroller,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (name) {
+                if (name!.isEmpty) {
+                  return 'il campo name non può essere vuoto';
+                } else if (name.startsWith(RegExp('[0-9]'))) {
+                  return 'name non può cominciare con numeri';
+                } else if (name.contains(' ')) {
+                  return 'name non può contenere spazi';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            loginform(
               readonly: true,
               controller: emailcontroller,
               labeltext: 'Email',
@@ -151,7 +294,7 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
     );
   }
 
-  Future<dynamic> bottomsheet(BuildContext context) async {
+  Future<bool?> bottomsheet(BuildContext context) async {
     return await showModalBottomSheet(
         context: context,
         builder: (_) {
@@ -159,18 +302,24 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
             mainAxisSize: MainAxisSize.min,
             children: [
               ListTile(
-                leading: Icon(Icons.camera),
-                title: Text('Scatta nuova'),
-                onTap: () {},
+                leading: const Icon(Icons.camera),
+                title: const Text('Scatta nuova'),
+                onTap: () {
+                  cameraselected = true;
+                  Navigator.pop(context, cameraselected);
+                },
               ),
               ListTile(
-                leading: Icon(Icons.photo_library),
-                title: Text('Scegli esistente'),
-                onTap: () {},
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Scegli esistente'),
+                onTap: () {
+                  cameraselected = false;
+                  Navigator.pop(context, cameraselected);
+                },
               ),
               widget.toupdate.imagelink != null
                   ? ListTile(
-                      leading: Icon(Icons.delete),
+                      leading: const Icon(Icons.delete),
                       title: const Text(
                         'Rimuovi foto profilo',
                         style: TextStyle(color: Colors.red),
@@ -182,5 +331,53 @@ class _editcustomerscreenState extends State<editcustomerscreen> {
             ],
           );
         });
+  }
+
+  int userchangedvalues() {
+    int fieldschanged = 0;
+    //check per evitare di fare chiamata http se valori form non sono modificati. provvisoria fino a quando non trovero modo di accedere direttamente al form
+    if (widget.toupdate.username?.toLowerCase() !=
+        usernamecontroller.text.toLowerCase().replaceAll(' ', '')) {
+      fieldschanged += 1;
+    } else if (widget.toupdate.surname?.toLowerCase() !=
+        surnamecontroller.text.toLowerCase().replaceAll(' ', '')) {
+      fieldschanged += 1;
+    } else if (widget.toupdate.name?.toLowerCase() !=
+        namecontroller.text.toLowerCase().replaceAll(' ', '')) {
+      fieldschanged += 1;
+    }
+    return fieldschanged;
+  }
+
+  Customermodel copyoldifnull(Customermodel newone) {
+    //oggetto che mi arriva da pagina precedente è to update. lo uso per fare controlli per vedere se sono cambiati valori
+    //nell'onchanged cambio valori di to update con valori controller usando un nuovo oggetto. i valori che non sono cambiati sono null e quindi i valori del nuovo oggetto sono
+    //quelli dell'oggetto precedente
+    //?? e per fare check se è null. ??= invece controlla se è null e fa assegnazione
+    newone.email ??= widget.toupdate.email;
+    newone.id ??= widget.toupdate.id;
+    newone.imagelink ??= widget.toupdate.imagelink;
+    newone.events ??= widget.toupdate.events;
+    newone.filename ??= widget.toupdate.filename;
+    newone.email ??= widget.toupdate.email;
+    newone.events ??= widget.toupdate.events;
+    newone.isdeleted ??= widget.toupdate.isdeleted;
+    newone.participations ??= widget.toupdate.participations;
+    newone.participations ??= widget.toupdate.participations;
+    newone.name ??= widget.toupdate.name;
+    newone.subscribedtonewsletter ??= widget.toupdate.subscribedtonewsletter;
+    newone.surname ??= widget.toupdate.surname;
+    newone.username ??= widget.toupdate.username;
+
+    return newone;
+  }
+
+  bool validatefileextension(XFile filetovalidate) {
+    List<String> acceptedextensions = ['.jpg', '.png', '.jpeg'];
+    if (!acceptedextensions.contains(extension(filetovalidate.name))) {
+      return false; //file in formato non valido
+    } else {
+      return true;
+    }
   }
 }
