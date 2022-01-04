04288165ba2cb4952b880489783e56fccf0d628e
Edoardo Remondini <edoardoremondini@studenti.itsincom.it>
2022-01-05 00:11:01 +0100

    aggiunto metodo refresh token che consente di ricevere un nuovo token jwt se si riceve unauthorized in seguito a scadenza token precedente

--- lib/services/authservice.dart
+++ lib/services/authservice.dart
@@ -10,46 +10,62 @@ import 'package:jwt_decoder/jwt_decoder.dart';
 
 const String apibaseurl = "http://10.0.2.2:5000/api";
 
-Future<bool> login(Loginmodel user) async {
-  Response r = await http.post(Uri.parse("$apibaseurl/Auth/Login"),
-      body: jsonEncode(user),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Accept': 'application/json'
-      });
-  if (r.statusCode == 200) {
-    Map<String, dynamic> jwt = jsonDecode(r.body);
-    //chiamare flutter secure storage per salvare token
-    await Tokenstorage.savetoken('token', jwt['token']);
-    return true;
-  } else {
-    return false;
-  }
-}
-
-Future<bool> register(Registermodel registermodel) async {
-  Response r = await http.post(Uri.parse("$apibaseurl/Auth/Register"),
-      body: jsonEncode(registermodel),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Accept': 'application/json'
-      });
-  if (r.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-Future<bool> istokenexpired() async {
-  String? token = await Tokenstorage.retrievetokenvalue('token');
-  if (token != null && token != "") {
-    bool hasExpired = JwtDecoder.isExpired(token);
-    if (hasExpired) {
+class Authservice {
+  Future<bool> login(Loginmodel user) async {
+    Response r = await http.post(Uri.parse("$apibaseurl/Auth/Login"),
+        body: jsonEncode(user),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        });
+    if (r.statusCode == 200) {
+      Map<String, dynamic> jwt = jsonDecode(r.body);
+      //chiamare flutter secure storage per salvare token
+      await Tokenstorage.savetoken('token', jwt['token']);
       return true;
     } else {
       return false;
     }
   }
-  return true;
+
+  Future<bool> register(Registermodel registermodel) async {
+    Response r = await http.post(Uri.parse("$apibaseurl/Auth/Register"),
+        body: jsonEncode(registermodel),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        });
+    if (r.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> istokenexpired() async {
+    String? token = await Tokenstorage.retrievetokenvalue('token');
+    if (token != null && token != "") {
+      bool hasExpired = JwtDecoder.isExpired(token);
+      if (hasExpired) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<String?> Refreshtoken({required String oldtoken}) async {
+    if (oldtoken.isEmpty) {
+      return null;
+    }
+    Response r = await http.post(
+        Uri.parse("$apibaseurl/Auth/refreshtoken/$oldtoken"),
+        body: oldtoken);
+    if (r.statusCode == 200) {
+      return r.body;
+    } else {
+      throw 'unable to refresh token';
+    }
+  }
 }
